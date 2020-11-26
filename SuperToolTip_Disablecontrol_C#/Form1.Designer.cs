f8b1d3cd496edcdedcda35c8169f7ee5d0409d0c
Backia Raj Kanagaraj <backiaraj.kanagaraj@syncfusion.com>
2020-11-26 16:26:36 +0530

    Modified the sample

--- SuperToolTip_Disablecontrol_C#/Form1.Designer.cs
+++ SuperToolTip_Disablecontrol_C#/Form1.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace Supertooltip
+﻿using Syncfusion.Windows.Forms;
+
+namespace Supertooltip
 {
     partial class Form1
     {
@@ -28,14 +30,45 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.buttonAdv1 = new Syncfusion.Windows.Forms.ButtonAdv();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // buttonAdv1
+            // 
+            this.buttonAdv1.Appearance = Syncfusion.Windows.Forms.ButtonAppearance.Office2016DarkGray;
+            this.buttonAdv1.BeforeTouchSize = new System.Drawing.Size(84, 23);
+            this.buttonAdv1.Enabled = false;
+            this.buttonAdv1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonAdv1.Location = new System.Drawing.Point(286, 96);
+            this.buttonAdv1.Name = "buttonAdv1";
+            this.buttonAdv1.Size = new System.Drawing.Size(84, 23);
+            this.buttonAdv1.TabIndex = 0;
+            this.buttonAdv1.Text = "ShowToolTip";
+            this.buttonAdv1.ThemeName = "Office2016DarkGray";
+            this.buttonAdv1.UseVisualStyle = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(69, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Hover mouse on button to show SuperToolTip";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CaptionBarColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(158)))), ((int)(((byte)(218)))));
+            this.CaptionButtonColor = System.Drawing.Color.White;
+            this.CaptionForeColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(533, 292);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonAdv1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SuperToolTip";
@@ -45,6 +78,8 @@
         }
 
         #endregion
+        private ButtonAdv buttonAdv1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
